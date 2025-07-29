a9819bfa32d5940578e929fcfc5309907b74674b
ks_guo <kguo1@smail.uni-koeln.de>
2025-07-29 23:52:10 +0200

    initial frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Welcome to FinSight</h1>
+      <p>Your personal finance assistant.</p>
+      {/* Add more components or features here */}
+      
+    </>
+  )
+}
+
+export default App
